1eefc2441c07426731bc14a5bc78c4ea5f4fbcfe
mikhail <miksolodovnikov@gmail.com>
2018-02-13 18:43:01 +0500

    Statrt to implement client side application which will monitor directory changes and send changes to server.

---
+++ client/dsync_cl.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import signal
+import argparse
+import logging
+import time
+from distutils.dir_util import copy_tree
+
+from dir_sync.directory_monitor import DirectoryMonitor
+
+OLD_FILES_STORAGE_NAME = '.ds'
+
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+
+def prepare_directory(directory):
+    '''Prepare directory to be tracked. Check if directory exists, if not
+       create new directory. Create hidden .ds directory to store "old" copy
+       of files, to have chance get differences on changes when it occures to
+       transfer those changes to rempote soket.
+    '''
+    if not os.path.exists(directory) or not os.path.isdir(directory):
+        os.mkdir(directory)
+    old_files_storage = os.path.join(directory, OLD_FILES_STORAGE_NAME)
+    copy_tree(directory, old_files_storage)
+
+
+def main():
+    logger = logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(
+        description="dsyc-cl - client side application which can track the " +
+                    "state of the directory and transfer changes to remote " +
+                    "host to keep in sync remote directory"
+    )
+    parser.add_argument('-d',
+                        help="path to directory which should be syncronised",
+                        type=str)
+    parser.add_argument('-h',
+                        help="host to send changes on directory")
+    parser.add_argument('-p',
+                        help="port number")
+    parser.add_argument('--polling_time', default=1,
+                        help='time to retreive info about events in directory')
+    args = parser.parse_args()
+
+    directory = args.d
+    host = args.h
+    port = args.p
+    polling_time = args.polling_time
+
+    logger.info('dsync_cl starts...')
+    prepare_directory(directory)
+    dm = DirectoryMonitor(directory)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    interrupted = False
+    while True:
+        events = dm.get_events()
+
+        time.sleep(polling_time)
+        if interrupted:
+            logger.info('dsync_cl finished...')
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ client/test_dsync_cl.py
@@ -0,0 +1,43 @@
+from unittest import TestCase
+import os
+
+from dir_sync.client.dsync_cl import prepare_directory
+
+
+class PrepareDirectoryFunctionTest(TestCase):
+
+    def test_on_non_existed_directory(self):
+        dir_name = 'tmpt'
+
+        prepare_directory(dir_name)
+
+        self.assertTrue(os.path.exists(dir_name))
+        self.assertTrue(os.path.isdir(dir_name))
+
+        self.assertTrue(os.path.exists(dir_name+'/.ds'))
+        self.assertTrue(os.path.isdir(dir_name+'/.ds'))
+
+        os.system('rm -rf '+dir_name)
+
+    def test_on_non_empty_directory(self):
+        dir_name = 'tmp'
+
+        os.mkdir(dir_name)
+        os.system('touch tmp/1.txt')
+        os.system('touch tmp/2.txt')
+        os.mkdir('tmp/tst')
+        os.system('touch tmp/tst/1.txt')
+        os.system('touch tmp/tst/2.txt')
+
+        prepare_directory(dir_name)
+
+        self.assertTrue(os.path.exists(dir_name))
+        self.assertTrue(os.path.isdir(dir_name))
+
+        self.assertTrue(os.path.exists(dir_name+'/.ds'))
+        self.assertTrue(os.path.isdir(dir_name+'/.ds'))
+
+        self.assertEqual(len(os.listdir('tmp/.ds')), 3)
+        self.assertEqual(len(os.listdir('tmp/.ds/tst')), 2)
+
+        os.system('rm -rf '+dir_name)
