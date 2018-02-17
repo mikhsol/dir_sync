cf63a2431ce344ee1e0aaa19ab3ec953db674720
mikhail <miksolodovnikov@gmail.com>
2018-02-17 17:09:14 +0500

    Finish sender and receiver state machines.

--- receiver.py
+++ receiver.py
@@ -22,54 +22,59 @@ class SocketReceiver:
         self.ep = None
 
     def handle(self):
+        logger.info('Wait for new connection...')
+        conn, addr = self.sock.accept()
         try:
-            logger.info('Wait for new connection...')
-            conn, addr = self.sock.accept()
             event = conn.recv(self.MSG_SIZE)
             if event == se.INIT:
                 conn.send(se.READY)
                 logger.info('Ready to receive init directory...')
                 dir_name = os.path.join(self.TRACKING_DIRS,
-                                        str(conn.recv(self.MSG_SIZE)))
+                                        conn.recv(self.MSG_SIZE)
+                                            .decode('utf-8'))
                 logger.info('Init directory: {}'.format(dir_name))
                 self.ep = ServerEventProcessor(dir_name)
             elif event == se.FILE:
                 conn.send(se.READY)
                 logger.info('Ready to receive file...')
-                file_path = os.path.join(self.TRACKING_DIRS,
-                                         str(conn.recv(self.MSG_SIZE)))
+                path = conn.recv(self.MSG_SIZE).decode('utf-8')
+                file_path = os.path.join(self.TRACKING_DIRS, path)
                 logger.info('File path: {}'.format(file_path))
-                file_name = str(conn.recv(self.MSG_SIZE))
+                conn.send(se.READY)
+                file_name = conn.recv(self.MSG_SIZE).decode('utf-8')
                 logger.info('File name: {}'.format(file_name))
                 if not os.path.exists(file_path):
                     os.makedirs(file_path, exist_ok=True)
                 file_ = os.path.join(file_path, file_name)
-                os.system('touch '.format(file_))
+                os.system('touch {}'.format(file_))
                 logger.info('File {} was created'.format(file_name))
                 with open(file_, 'wb') as f:
+                    conn.send(se.READY)
                     d = conn.recv(self.MSG_SIZE)
-                    while d:
+                    while d != se.FINISHED:
                         f.write(d)
                         d = conn.recv(self.MSG_SIZE)
                 logger.info('File {} was deliverd.'.format(file_name))
             elif event == se.DIRECTORY:
                 conn.send(se.READY)
                 logger.info('Ready to receive new directory path...')
-                path = str(conn.recv(self.MSG_SIZE))
+                path = conn.recv(self.MSG_SIZE).decode('utf-8')
                 path = os.path.join(self.TRACKING_DIRS, path)
                 if not os.path.exists(path):
                     os.makedirs(path, exist_ok=True)
                     logger.info('New directory {} was created.'.format(path))
-                logger.info('Directory {} already exists.'.format(path))
+                else:
+                    logger.info('Directory {} already exists.'.format(path))
             elif event == se.EVENT:
                 conn.send(se.READY)
                 logger.info('Ready to receive new event...')
                 data = b''
                 d = conn.recv(self.MSG_SIZE)
-                while d:
+                while d != se.FINISHED:
                     data += d
                     d = conn.recv(self.MSG_SIZE)
                 event = pickle.loads(data)
+                logger.info('Evetnt {} was deliverd ...'.format(event))
                 self.ep.process(event)
             else:
                 # TODO: NoSuchEventException
--- sender.py
+++ sender.py
@@ -35,9 +35,10 @@ class StubSender(AbstractSenderStrategy):
 class SocketSender(AbstractSenderStrategy):
     MSG_SIZE = 1024
 
-    def __init__(self, host='localhost', port='6666'):
+    def __init__(self, host='localhost', port='6666', excluded_dirs=[]):
         self.host = host
         self.port = port
+        self.excluded_dirs = excluded_dirs
 
     def init(self, directory):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -56,39 +57,43 @@ class SocketSender(AbstractSenderStrategy):
         self.send_directory(directory)
         logger.info('Init success...')
 
-
     def send_file(self, file_path, file_name):
+        file_ = os.path.join(file_path, file_name)
+        if not os.path.exists(file_):
+            logger.error('No such file "{}"'.format(file_))
+            # TODO: raise NoSuchFileException
+        if not os.path.isfile(file_):
+            logger.error('Not a file "{}"'.format(file_))
+            # TODO: raise NotAFileException
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.host, self.port))
             s.send(se.FILE)
             status = s.recv(self.MSG_SIZE)
             if status == se.READY:
-                file_ = os.path.join(file_path, file_name)
-                if not os.path.exists(file_):
-                    logger.error('No such file "{}"'.format(file_))
-                    # TODO: raise NoSuchFileException
-                if not os.path.isfile(file_):
-                    logger.error('Not a file "{}"'.format(file_))
-                    # TODO: raise NotAFileException
-                logger.info('Server ready to receive' +
+                logger.info('Server ready to receive ' +
                             'file: "{}", start transmition.'.format(file_))
                 s.send(bytearray(file_path, 'utf-8'))
-                s.send(bytearray(file_name, 'utf-8'))
-                with open(file_path, 'rb') as f:
-                    d = f.read(self.MSG_SIZE)
-                    while d:
-                        s.send(d)
+                status = s.recv(self.MSG_SIZE)
+                if status == se.READY:
+                    s.send(bytearray(file_name, 'utf-8'))
+                status = s.recv(self.MSG_SIZE)
+                if status == se.READY:
+                    with open(file_, 'rb') as f:
                         d = f.read(self.MSG_SIZE)
+                        while d:
+                            s.send(d)
+                            d = f.read(self.MSG_SIZE)
+                s.send(se.FINISHED)
                 status = s.recv(self.MSG_SIZE)
                 if status == se.OK:
                     logger.info('File "{}" was delivered.'.format(file_name))
-                # conn.send(se.FINISH)
                 else:
                     # TODO: rise FileTransferError
                     pass
             else:
                 logger.error('Does not support such server response "{}"'
-                             .format(str(status)))
+                             .format(str(status).decode('utf-8')))
                 raise NotImplementedError
 
     def send_directory(self, path):
@@ -118,11 +123,11 @@ class SocketSender(AbstractSenderStrategy):
 
         for el in os.listdir(path):
             el_path = os.path.join(path, el)
-            if os.path.isdir(el_path):
+            if os.path.isdir(el_path) and el_path not in self.excluded_dirs:
                 self.send_directory(el_path)
             elif os.path.isfile(el_path):
-                self.send_file(el_path, el)
-            else:
+                self.send_file(path, el)
+            elif el_path not in self.excluded_dirs:
                 logger.warning('Does not support type of "{}"'.format(el_path))
 
     def send_event(self, event):
@@ -131,7 +136,16 @@ class SocketSender(AbstractSenderStrategy):
             s.send(se.EVENT)
             status = s.recv(self.MSG_SIZE)
             if status == se.READY:
-                s.sendall(pickle.dumps(event))
+                logger.info('Start send event {}...'.format(event))
+                data = pickle.dumps(event)
+                msg_idx = 0
+                end_idx = 0
+                while end_idx < len(data):
+                    start_idx = msg_idx * self.MSG_SIZE
+                    end_idx = (msg_idx + 1) * self.MSG_SIZE
+                    s.send(data[start_idx:end_idx])
+                    msg_idx += 1
+                s.send(se.FINISHED)
                 status = s.recv(self.MSG_SIZE)
                 if status == se.OK:
                     logger.info('Event was sent to remote host...')
