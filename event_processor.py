bb74f9ee8ff0c90a9ac2ac48856e742a20d86338
mikhail <miksolodovnikov@gmail.com>
2018-02-17 17:01:48 +0500

    Add some logging and fix few minor bugs. Provide special dir for tracking folders for serverside processor.

--- event_processor.py
+++ event_processor.py
@@ -2,6 +2,10 @@ import os
 from distutils.dir_util import copy_tree
 from inotify_simple import flags
 from files_comporator import FilesComporator as fc
+import logging
+
+logger_cl = logging.getLogger('client')
+logger_srv = logging.getLogger('server')
 
 
 class EventProcessor:
@@ -24,6 +28,7 @@ class ClientEventProcessor(EventProcessor):
         self.sender = sender
 
     def process(self, event):
+        logger_cl.debug("EP: {}".format(event))
         flags_ = flags.from_mask(event.mask)
         # Generate path to local copy for new object to have chance
         # get changes on object modify event later.
@@ -33,6 +38,8 @@ class ClientEventProcessor(EventProcessor):
 
         if flags.ISDIR in flags_:
             if flags.CREATE in flags_:
+                logger_cl.info('DIR: {}'.format(event.directory.path))
+                logger_cl.info('DS_DIR: {}'.format(ds_path_to_new_object))
                 copy_tree(event.directory.path, ds_path_to_new_object)
                 self.sender.send_directory(event.directory.path)
             elif flags.DELETE in flags_:
@@ -41,8 +48,9 @@ class ClientEventProcessor(EventProcessor):
                 os.system('rm -r {}'.format(path_to_delete))
                 self.sender.send_event(event)
             else:
+                logger_cl.error('Can not handle "{}" event'.format(flags_))
                 # TODO: add proper exception
-                raise NotImplemented
+                # raise NotImplementedError
         else:
             file_path = os.path.join(event.directory.path, event.name)
             ds_file_path = os.path.join(ds_path_to_new_object, event.name)
@@ -57,19 +65,18 @@ class ClientEventProcessor(EventProcessor):
                 event.diff = diff
                 self.sender.send_event(event)
             elif flags.DELETE in flags_:
+                logger_cl.info('DELETE: {}'.format(ds_file_path))
                 if os.path.exists(ds_file_path):
-                    os.system('rm {}'.format(ds_file_path))
+                    os.system('rm -r {}'.format(ds_file_path))
                 self.sender.send_event(event)
-            elif flags.IGNORED in flags_:
-                # IGNORED flag automatically generated on delete event, do not
-                # need to process it.
-                pass
             else:
                 # TODO: add proper exception
-                raise NotImplemented
+                logger_cl.error('Can not handle "{}" event'.format(flags_))
+                # raise NotImplementedError
 
 
 class ServerEventProcessor(EventProcessor):
+    TRACKING_DIRS = 'tracking_dirs'
 
     def __init__(self, directory=None):
         if not directory:
@@ -84,13 +91,18 @@ class ServerEventProcessor(EventProcessor):
 
         if flags.ISDIR in flags_:
             if flags.DELETE in flags_:
-                if os.path.exists(event.directory.path):
-                    os.system('rm -r {}'.format(event.directory.path))
+                path = os.path.join(self.TRACKING_DIRS, event.directory.path,
+                                    event.name)
+                print(path)
+                logger_srv.info('DELETE: {}'.format(path))
+                if os.path.exists(path):
+                    os.system('rm -r {}'.format(path))
             else:
                 # TODO: add proper exception
                 raise NotImplemented
         else:
-            path = os.path.join(event.directory.path, event.name)
+            path = os.path.join(self.TRACKING_DIRS, event.directory.path,
+                                event.name)
             if flags.MODIFY in flags_:
                 fc.patch(path, event.diff)
             elif flags.DELETE in flags_:
@@ -98,4 +110,4 @@ class ServerEventProcessor(EventProcessor):
                     os.system('rm {}'.format(path))
             else:
                 # TODO: add proper exception
-                raise NotImplemented
+                raise NotImplementedError
